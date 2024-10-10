3c4cf290e113d33ca43860a88c8d97b98145c890
Diana Zhiliakova <zhiliackova2009@yandex.ru>
2024-10-10 13:01:57 +0300

    add eslint

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import globals from 'globals';
+import js from "@eslint/js";
+import importPlugin from 'eslint-plugin-import';
+
+export default [
+  js.configs.recommended,
+   {
+      languageOptions: {
+        globals: {
+          ...globals.node,
+          ...globals.jest,
+        },
+        parserOptions: {
+          // Eslint doesn't supply ecmaVersion in `parser.js` `context.parserOptions`
+          // This is required to avoid ecmaVersion < 2015 error or 'import' / 'export' error
+          ecmaVersion: 'latest',
+          sourceType: 'module',
+        },
+      },
+      plugins: { import: importPlugin },
+      rules: {
+        ...importPlugin.configs.recommended.rules,
+      },
+    },
+    
+{
+  rules: {
+    'semi': ['error', 'always'],
+    'no-console': 'off',
+    'no-unused-vars': 'error',
+    'import/extensions': 'off',
+  },
+},
+];
