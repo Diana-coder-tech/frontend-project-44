4824997a4e087b1ffd3e121230aca07e56cff49f
Diana Zhiliakova <zhiliackova2009@yandex.ru>
2024-10-17 12:16:06 +0300

    fix linter

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,17 +1,16 @@
-import runGame from "../index.js";
-import getRandomNumber from "../utils.js";
+import runGame from '../index.js';
+import getRandomNumber from '../utils.js';
 
 const isEven = (num) => num % 2 === 0;
 
 // Описание игры для вывода в приветствии
-const description =
-  'Answer "yes" if the number is even, otherwise answer "no".';
+const description = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 // Функция для генерации раунда игры (вопрос и правильный ответ)
 
 const generateRound = () => {
   const question = getRandomNumber();
-  const correctAnswer = isEven(question) ? "yes" : "no";
+  const correctAnswer = isEven(question) ? 'yes' : 'no';
 
   return [question, correctAnswer];
 };
